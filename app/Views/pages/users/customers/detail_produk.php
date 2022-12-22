03aa8848dbd6c591173a101e1ee5665891ead249
Bukhari_Inst <bukhari414647@gmail.com>
2022-12-22 07:50:21 +0700

    Update detail_produk.php

--- app/Views/pages/users/customers/detail_produk.php
+++ app/Views/pages/users/customers/detail_produk.php
@@ -5,13 +5,37 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="<?= base_url() ?>/assets/dist/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="<?= base_url() ?>/assets/dist/assets/favicon/favicon-16x16.png">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
     <!-- Vendor CSS -->
     <link rel="stylesheet" href="<?= base_url() ?>/assets/dist/assets/css/libs.bundle.css" />
 
     <!-- Main CSS -->
     <link rel="stylesheet" href="<?= base_url() ?>/assets/dist/assets/css/theme.bundle.css" />
 
-    <?= $this->include('parts/head'); ?>
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url() ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="<?= base_url() ?>/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url() ?>/assets/css/style.css" rel="stylesheet">
+
     <!-- ======= Header ======= -->
     <?= $this->include('parts/header'); ?>
 
